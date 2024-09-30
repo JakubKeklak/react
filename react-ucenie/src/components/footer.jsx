38b214709e73798c63146c82886c830c9d7fdca4
Jakub Keklak <keklak.jakub122@gmail.com>
2024-09-30 22:43:47 +0200

    footer: markup

---
+++ react-ucenie/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css'
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer__content">
+        <hr className="footer__divider" />
+        <span className="footer__logo">drevoasluzby</span>
+        <hr className="footer__divider" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
